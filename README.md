57fa6410563c3bb1c121578a3e22c8974132721a
Olaf Mirel <o.mirel@macrocom.de>
2017-04-24 13:39:58 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+property_manager
+================
+
+A Symfony project created on April 24, 2017, 11:01 am.
---
+++ src/AppBundle/Admin/BlogPostAdmin.php
@@ -0,0 +1,39 @@
+<?php
+// src/AppBundle/Admin/BlogPostAdmin.php
+namespace AppBundle\Admin;
+
+use Sonata\AdminBundle\Admin\AbstractAdmin;
+use Sonata\AdminBundle\Datagrid\ListMapper;
+use Sonata\AdminBundle\Datagrid\DatagridMapper;
+use Sonata\AdminBundle\Form\FormMapper;
+
+class BlogPostAdmin extends AbstractAdmin
+{
+    protected function configureFormFields(FormMapper $formMapper)
+    {
+        $formMapper
+            ->with('Content', array('class' => 'col-md-9'))
+            ->add('title', 'text')
+            ->add('body', 'textarea')
+            ->end()
+
+            ->with('Meta data', array('class' => 'col-md-3')
+            ->add('category', 'sonata_type_model', array(
+                'class' => 'AppBundle\Entity\Category',
+                'property' => 'name',
+            ))
+            ->end()
+        ;
+    }
+
+    protected function configureDatagridFilters(DatagridMapper $datagridMapper)
+    {
+        $datagridMapper->add('name');
+    }
+
+    protected function configureListFields(ListMapper $listMapper)
+    {
+        $listMapper->addIdentifier('name');
+    }
+}
+
