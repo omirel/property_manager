384cc52835a12a0f0b59948ce05270be1d025869
Olaf Mirel <o.mirel@macrocom.de>
2017-04-25 16:26:12 +0200

    Modified ApartmentAdmin

--- src/AppBundle/Admin/ApartmentAdmin.php
+++ src/AppBundle/Admin/ApartmentAdmin.php
@@ -12,26 +12,28 @@ class ApartmentAdmin extends AbstractAdmin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->with('Content', array('class' => 'col-md-4'))
-            ->add('fullName', 'text')
-            ->add('shortName', 'text')
-            ->end()
+            ->tab('Tab 1', array('class' => 'col-md-6'))
+                ->with('Content', array('class' => 'col-md-4'))
+                    ->add('fullName', 'text')
+                    ->add('shortName', 'text')
+                    ->end()
 
-            ->with('Facts', array('class' => 'col-md-4'))
-            ->add('floorNumber', 'integer')
-            ->add('apartmentNumber', 'text')
-            ->add('roomCount', 'integer')
-            ->add('bathroomCount', 'integer')
-            ->add('bedroomCount', 'integer')
+                    ->with('Meta data', array('class' => 'col-md-6'))
+                    ->add('building', 'sonata_type_model', array(
+                        'class' => 'AppBundle\Entity\Building',
+                    ))
+                    ->add('apartmentType', 'sonata_type_model', array(
+                        'class' => 'AppBundle\Entity\ApartmentType',
+                    ))
+                ->end()
             ->end()
-
-            ->with('Meta data', array('class' => 'col-md-4'))
-            ->add('building', 'sonata_type_model', array(
-                'class' => 'AppBundle\Entity\Building',
-            ))
-            ->add('apartmentType', 'sonata_type_model', array(
-                'class' => 'AppBundle\Entity\ApartmentType',
-            ))
+            ->tab('Tab 2')
+                ->with('Facts', array('class' => 'col-md-6'))
+                ->add('floorNumber', 'integer')
+                ->add('apartmentNumber', 'text')
+                ->add('roomCount', 'integer')
+                ->add('bathroomCount', 'integer')
+                ->add('bedroomCount', 'integer')
             ->end()
         ;
     }
